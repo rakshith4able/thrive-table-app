198678045ad9e5fea409e0470168a13fa7e59760
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2025-05-21 20:52:54 -0400

    removed duplicate file

--- src/components/table/virtualized/VirtualizedTable.tsx
+++ src/components/table/virtualized/VirtualizedTable.tsx
@@ -3,8 +3,8 @@ import { type Table, type ColumnOrderState } from "@tanstack/react-table";
 import { useVirtualizedTable } from "../../../hooks/useVirtualizedTable";
 import { useColumnDragDrop } from "../../../hooks/useColumnDragDrop";
 import VirtualizedContainer from "./VirtualizedContainer";
-import VirtualizedFooter from "./VirtualizedFooter";
 import VirtualizedTitle from "./VirtualizedTitle";
+import TableFooter from "../TableFooter";
 
 interface VirtualizedTableProps<T> {
   table: Table<T>;
@@ -61,7 +61,7 @@ const VirtualizedTable = <T extends { id: string | number }>({
       />
 
       <div className="px-2 sm:px-0">
-        <VirtualizedFooter
+        <TableFooter
           totalCount={rows.length}
           virtualRowsCount={virtualRows.length}
         />
